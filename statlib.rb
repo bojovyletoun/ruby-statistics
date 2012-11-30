be5ce78b485da470bb814ddb84c83dbd8cc68cd1
bojovyletoun <avngrg+origin@gmail.com>
2012-11-30 01:30:13 +0100

    fixed epic error ar.inject{} ---> ar.inject(0){}

--- statlib.rb
+++ statlib.rb
@@ -9,42 +9,21 @@ def gausnumero(ndis=6)
 end
 
 module Enumerable
-	def sum;	inject { |mem, var| mem+var } end
-	def avg;	sum.to_f/(size) end
-	def avgs;	sum.to_f/(size-1) end
-	def stdev;	t=avg; (inject  do |mem,i| mem+(i-t)**2 end/(size-1))**0.5 end
+	def sum;    inject { |mem, var| mem+var } end
+	def avg;    sum.to_f/(size) end
+	def avgs;   sum.to_f/(size-1) end
+	def stdev;  t=avg; (inject(0) do |mem,i|  mem+(i-t)**2 end/(size))**0.5 end
+	def stdevs;  t=avg; (inject(0) do |mem,i|  mem+(i-t)**2 end/(size-1))**0.5 end
 	# There was slow down!!!! (i-t) <-----> (i-avg)
-
-	def hist(steps,sym)
-		a=Hash.new 0		
-		each do |res|
-			a[(res*steps).ceil.to_f/steps]+=1
-			a[((1-res)*steps).ceil.to_f/steps]+=1 if sym # buggy, if dev<1
-		end
-		a
-	end
-
-	#alternate grouping
-	def hist2(steps)
-		d=max-min
-		return group_by {|e| (e.to_f/d*steps).floor*d/steps }.map{|e,v| [e,v.size]}
-	end
-
-	def export(ext=nil,file=File.basename($0).sub(/\.rb$/,''))
-		open file << (ext ? ?. << ext : '') ,?w do |f|
-			each do |step, vals=nil|
-				f << "#{step}, #{vals}\n"
-			end
-		end
-	end
 
 
 	def inform
-		a,s = avg, stdev
-		puts "AVG: %s" % [a]
-		puts "s  : %s" % [s]
-		puts "s^2: %s" % [s**2]
-		puts "var: %s %%" % [s/a.abs*100]
+		a,s,ss = avg, stdev,stdevs
+		puts "AVG : %s" % [a]
+		puts "s   : %s" % [s]
+		puts "ss  : %s" % [ss]
+		puts "var : %s %%" % [s/a.abs*100]
+		puts "vars: %s %%" % [ss/a.abs*100]
 	end
 
 	def proceed (steps=100,sym=false)
@@ -62,4 +41,26 @@ module Enumerable
 		export ?d
 	end
 
+	def export(ext=nil,file=File.basename($0).sub(/\.rb$/,''))
+		open file << (ext ? ?. << ext : '') ,?w do |f|
+			each do |step, vals=nil|
+				f << "#{step}, #{vals}\n"
+			end
+		end
+	end
+
+	def hist(steps,sym)
+		a=Hash.new 0        
+		each do |res|
+			a[(res*steps).ceil.to_f/steps]+=1
+			a[((1-res)*steps).ceil.to_f/steps]+=1 if sym # buggy, if dev<1
+		end
+		a
+	end
+
+	#alternate grouping
+	def hist2(steps)
+		d=max-min
+		return group_by {|e| (e.to_f/d*steps).floor*d/steps }.map{|e,v| [e,v.size]}
+	end
 end
